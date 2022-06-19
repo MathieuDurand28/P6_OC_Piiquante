679d07e80ec6e8864511cfd6323f8bc0b454f053
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2022-06-19 14:52:53 +0200

    Mise en place connexion mongoAtlas

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 
-mongoose.connect('mongodb+srv://Mathieu:9879@cluster0.oiyjr.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Piiquante:XXX@cluster0.cl69o.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
