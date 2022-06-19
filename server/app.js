338d029651ad2bff49f2f7c7e43be89e5360e9a5
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2022-06-19 14:50:08 +0200

    complement gitignore.

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
 
-const app = express()
 
-app.use((req, res, next ) => {
-  console.log("requete reçues !")
-  next()
-})
-
-app.use((req, res, next ) => {
-  res.status(201)
-  next()
-})
+mongoose.connect('mongodb+srv://Mathieu:9879@cluster0.oiyjr.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
-  res.json({message: "Votre requête a bien été reçue !"})
-  next()
-})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-app.use((req, res) => {
-  console.log("réponse envoyée")
-})
+app.use(express.json());
 
-module.exports = app
+module.exports = app;
+
+
+
 
 
